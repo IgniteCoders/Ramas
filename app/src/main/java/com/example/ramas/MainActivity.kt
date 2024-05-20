5afb7dbd3ec515512e0348d4b5e0cb3f0cade8de
Mansour <theboom.kajas@gmail.com>
2024-05-20 11:25:10 +0200

    Hemos añadido pantalla 1

--- app/src/main/java/com/example/ramas/MainActivity.kt
+++ app/src/main/java/com/example/ramas/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.ramas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navigationButton1: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navigationButton1 = findViewById(R.id.button1)
 
+        navigationButton1.setOnClickListener {
+            var intent: Intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
