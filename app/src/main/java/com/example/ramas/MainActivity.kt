3b5fc658eae285a0320189048d2bf547bc95e604
Mansour <theboom.kajas@gmail.com>
2024-05-20 11:33:14 +0200

    Ahora tenemos pantalla de opciones

--- app/src/main/java/com/example/ramas/MainActivity.kt
+++ app/src/main/java/com/example/ramas/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.ramas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var optionsButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        optionsButton = findViewById(R.id.options)
 
+        optionsButton.setOnClickListener {
+            var intent: Intent = Intent(this, OptionsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
